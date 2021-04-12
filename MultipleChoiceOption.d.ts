edb46a216d197f8f816099e3379690b9b1c876b6
Calvin <calvindelloro@mail.de>
2021-04-12 19:54:54 +0200

    only 3 left

--- MultipleChoiceOption.d.ts
+++ MultipleChoiceOption.d.ts
@@ -1,7 +1,9 @@
 namespace SoftwareDesign {
     export class MultipleChoiceOption {
 
-        private option: String
+        private option: string
         private correct: boolean
+
+        constructor(_option: string, _correct: QuestionType);
     }
 }
