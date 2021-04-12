3ac3c704853234f0098eb1adb688e86e7c09c21a
Calvin <calvindelloro@mail.de>
2021-04-12 19:41:59 +0200

    asdf

--- Quiz.d.ts
+++ Quiz.d.ts
@@ -1,10 +1,44 @@
 namespace SoftwareDesign {
     export class Quiz {
 
-        private public: Boolean
-        private title: String
-        private questions: question[]
-        private userId: number
+      
+        private _public: boolean
+        public get public(): boolean {
+            return this._public
+        }
+        public set public(value: boolean) {
+            this._public = value
+        }
+
+        private _title: string
+        public get title(): string {
+            return this._title
+        }
+        public set title(value: string) {
+            this._title = value
+        }
+
+
+        private _questions: question[]
+        public get questions(): question[] {
+            return this._questions
+        }
+        public set questions(value: question[]) {
+            this._questions = value
+        }
+
+
+        private _userId: number
+        public get userId(): number {
+            return this._userId
+        }
+        public set userId(value: number) {
+            this._userId = value
+        }
+
+        //---------------------------------------------------
+        //------------------METHODS--------------------------
+        //---------------------------------------------------
 
         public addQuestion(): void
         public finishQuiz(): void
