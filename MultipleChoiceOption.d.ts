227bcec5432a7e65f1fac87388020b1639f9f8d5
Calvin <calvindelloro@mail.de>
2021-04-12 19:57:39 +0200

    asdf

--- MultipleChoiceOption.d.ts
+++ MultipleChoiceOption.d.ts
@@ -1,8 +1,20 @@
 namespace SoftwareDesign {
     export class MultipleChoiceOption {
 
-        private option: string
-        private correct: boolean
+        private _option: string
+        public get option(): string {
+            return this._option
+        }
+        public set option(value: string) {
+            this._option = value
+        }
+        private _correct: boolean
+        public get correct(): boolean {
+            return this._correct
+        }
+        public set correct(value: boolean) {
+            this._correct = value
+        }
 
         constructor(_option: string, _correct: QuestionType);
     }
--- NumberAnswer.d.ts
+++ NumberAnswer.d.ts
@@ -1,7 +1,13 @@
 namespace SoftwareDesign {
     export class MultipleChoice {
 
-        private answer: number;
+        private _answer: number;
+        public get answer(): number {
+            return this._answer;
+        }
+        public set answer(value: number) {
+            this._answer = value;
+        }
 
         constructor(_answer: number);
     }
