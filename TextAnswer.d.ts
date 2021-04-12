9188d853d5b4696213e1637fb68faedaebb7a282
Calvin <calvindelloro@mail.de>
2021-04-12 20:05:32 +0200

    answer

--- TextAnswer.d.ts
+++ TextAnswer.d.ts
@@ -1,6 +1,14 @@
 namespace SoftwareDesign {
     export class TextAnswer {
 
-        private answer: String
+        private _answer: string;
+        public get answer(): string {
+            return this._answer;
+        }
+        public set answer(value: string) {
+            this._answer = value;
+        }
+
+        constructor(_answer: string);
     }
 }
