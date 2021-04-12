7122530d39c0eb3c203a800cd66258717b5e6cf4
Jonathan <jonathan.rissler@gmx.de>
2021-04-12 19:52:43 +0200

    User Register Getter Setter

--- UserRegister.d.ts
+++ UserRegister.d.ts
@@ -1,9 +1,23 @@
 namespace SoftwareDesign {
     export class UserRegister extends User {
 
-        private registerUserId: number
-        private quiz: Quiz[]
+        private _registerUserId: number;
+        public get registerUserId(): number {
+            return this._registerUserId
+        }
+        public set registerUserId(value: number) {
+            this._registerUserId = value
+        }
 
+        private _quiz: Quiz[];
+        public get quiz(): Quiz[] {
+            return this._quiz
+        }
+        public set quiz(value: Quiz[]) {
+            this._quiz = value
+        }
+
+        constructor(_registerUserId: number, _quiz: Quiz[]);
         public createQuiz(): Quiz;
     }
 }
