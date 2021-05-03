07bd1f0a280f7146ec83316bba8d68240608a664
pretteter <52958596+pretteter@users.noreply.github.com>
2021-05-03 18:55:33 +0200

    user

--- Eigenes Projekt/src/classes/User.d.ts
+++ Eigenes Projekt/src/classes/User.d.ts
@@ -2,8 +2,10 @@ namespace SoftwareDesign {
 
     export class User {
        public _sex: string;
+       public _userId: string;
 
         constructor(_userId: number, _userName: string, _password: string){
+            this._sex = _sex;
             this._userId = _userId;
         }
 
